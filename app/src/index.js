bb46c245095c69312d8d2ff5090cc7cd66338f95
Nelson Fernandez <nifersz@icloud.com>
2021-03-11 22:07:03 -0500

    Add missing implementation for Task 4 in 'index.js' file

--- app/src/index.js
+++ app/src/index.js
@@ -41,7 +41,10 @@ const App = {
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const tokenId = document.getElementById("lookid").value;
+    let starName = await lookUptokenIdToStarInfo(tokenId).send({from: this.account});
+    App.setStatus("Star name is " + starName + ".");    
   }
 
 };
